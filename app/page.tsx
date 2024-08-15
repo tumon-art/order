d17b4a8abc5d5d9cafbf930cdbdf86ff00206cb0
tumon <mofakhar0@gmail.com>
2024-08-16 04:55:06 +0600

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <Button> ShadeCDN Button!</Button>
+      <h1 className="font-extrabold text-4xl text-accent"> Order! </h1>
+      <Button variant="destructive"> ShadeCDN Button!</Button>
 
     </main>
   );
